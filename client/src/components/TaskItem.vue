5c94c7a7680fecd0c239e5d29b962f5eb5e44a92
Evgeny <zasetsky.evgeny@gmail.com>
2023-06-22 20:52:07 +0300

    styles

--- client/src/components/TaskItem.vue
+++ client/src/components/TaskItem.vue
@@ -1,18 +1,52 @@
 <template>
-  <li>
+  <li
+    class="task"
+    :class="{ 'task--completed': taskComputed.completed }"
+    @click="toggleCompletion"
+  >
     <input
       type="checkbox"
       :checked="taskComputed.completed"
-      @change="toggleCompletion"
+      @chang.stop="toggleCompletion"
+      class="task__input"
     />
-    {{ taskComputed.name }}
-    <button @click="startEditing">Edit</button>
-    <button @click="deleteTask">Delete</button>
+    <span
+      class="task__name"
+      :class="{ 'name--completed': taskComputed.completed }"
+      >{{ taskComputed.name }}</span
+    >
 
-    <div v-if="isEditing">
-      <input v-model="editTaskName" type="text" placeholder="New task name" />
-      <button @click="updateTask">Save</button>
-      <button @click="cancelEditing">Cancel</button>
+    <div class="task__buttons">
+      <button
+        @click.stop="startEditing"
+        class="task__button task__button--edit"
+      >
+        Edit
+      </button>
+      <button
+        @click.stop="deleteTask"
+        class="task__button task__button--delete"
+      >
+        Delete
+      </button>
+    </div>
+
+    <div v-if="isEditing" class="task__edit">
+      <input
+        v-model="editTaskName"
+        type="text"
+        placeholder="New task name"
+        class="task__edit-input"
+      />
+      <button @click.stop="updateTask" class="task__button task__button--save">
+        Save
+      </button>
+      <button
+        @click.stop="cancelEditing"
+        class="task__button task__button--cancel"
+      >
+        Cancel
+      </button>
     </div>
   </li>
 </template>
@@ -92,3 +126,96 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped lang="scss">
+.task {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border: 1px solid #ccc;
+  padding: 0.5em;
+  border-radius: 0.3125rem;
+  margin-bottom: 1em;
+  background-color: #f9f9f9;
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.05);
+  cursor: pointer;
+  transition: all 0.2s ease-out;
+
+  &--completed {
+    background-color: #e6e6e6;
+  }
+
+  &__input {
+    margin-right: 0.625rem;
+  }
+
+  &__name {
+    font-weight: bold;
+  }
+
+  .name--completed {
+    text-decoration: line-through;
+  }
+
+  &__button {
+    color: #fff;
+    padding: 0.3125rem;
+    border-radius: 0.3125rem;
+    margin-left: 0.3125rem;
+    cursor: pointer;
+    transition: all 0.2s ease-out;
+
+    &--edit {
+      background-color: #f1c40f;
+
+      &:hover {
+        background-color: #deb40c;
+      }
+    }
+
+    &--delete {
+      background-color: #e74c3c;
+
+      &:hover {
+        background-color: #ce3b2b;
+      }
+    }
+
+    &--save {
+      background-color: #2ecc71;
+      padding: 0.3125rem 0.625rem;
+      margin-right: 0.3125rem;
+
+      &:hover {
+        background-color: #29b865;
+      }
+    }
+
+    &--cancel {
+      background-color: #e74c3c;
+      padding: 0.3125rem 0.625rem;
+
+      &:hover {
+        background-color: #ce3b2b;
+      }
+    }
+  }
+
+  &__edit {
+    margin-top: 0.625rem;
+    display: flex;
+
+    &-input {
+      flex-grow: 1;
+      margin-right: 0.3125rem;
+      padding: 0.3125rem;
+      border-radius: 0.3125rem;
+      border: 1px solid #ccc;
+    }
+  }
+}
+
+.task:hover {
+  background-color: #f0efef;
+}
+</style>
--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <h1>Welcome to the Task Manager</h1>
-    <router-link to="/tasks">Go to tasks</router-link>
+  <div class="container">
+    <h1 class="home__title">Welcome to the Task Manager</h1>
+    <router-link class="home__link" to="/tasks">Go to tasks</router-link>
   </div>
 </template>
 
@@ -12,3 +12,33 @@ export default defineComponent({
   name: "HomePage",
 });
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  max-width: 75rem;
+  margin: 0 auto;
+  padding: 0 1rem;
+  margin-top: 5rem;
+}
+.home__title {
+  font-size: 2rem;
+}
+.home__link {
+  display: inline-block;
+  text-decoration: none;
+  background-color: #3498db;
+  color: #fff;
+  padding: 0.5rem 1rem;
+  border-radius: 0.3125rem;
+  text-align: center;
+  margin-top: 1rem;
+  transition: all 0.2s ease-out;
+}
+.home__link:hover {
+  background-color: #318ecd;
+}
+</style>
